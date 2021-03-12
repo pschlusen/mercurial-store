b886525c1a7ab6dac2708de306ff9eb757435daf
Pablo Schlusen <pablo.schlusen@gmail.com>
2021-03-12 17:29:34 -0300

    feat(gulp watch): add csspath param

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const log = require('gutil-color-log')
 var path = require('path')
 var fs = require('fs');
 var emoji = require('node-emoji')
+var argv = require('yargs').argv
+const {CMSHandler} = require('./app')
+
 
 const MODIFIED_PATH = __dirname + process.env.MODIFIED_PATH
 sass.compiler = require('sass')
@@ -16,12 +19,15 @@ gulp.task('sass', function () {
 });
  
 gulp.task('watch', function () {
-  gulp.watch('./src/sass/**/*.scss', gulp.task('sass')).on('change', function(file){
+  let CSSWatchPath = argv.csspath ? argv.csspath : process.env.WATCH_CSS_PATH
+
+  console.log('csshpath: '+CSSWatchPath);
+  gulp.watch(CSSWatchPath, gulp.task('sass')).on('change', function(file){
     fs.readFile(MODIFIED_PATH, (err, data) => {
       if (!err)
         if( data && data.toString().indexOf(file) < 0 )
           fs.appendFile(MODIFIED_PATH, `${path.join(__dirname, file)}\r\n`, (err) => {
-            !err && log('green', `${emoji.get(':fast_forward:')} File '${file}' queued on modifieds`) 
+            !err && log('green', `${emoji.get(':fast_forward:')} '${file}' queued on modifieds`) 
           })
     })
   })
